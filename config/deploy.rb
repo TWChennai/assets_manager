988059888a3de955de6bc910b1413c12934ab9a6
Selva <k.n.selvakumar@gmail.com>
2014-01-22 13:34:57 +0530

    [Selva/RDX] After update code, migrate database

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,7 @@ role :db,  "10.10.5.17"
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
+after "deploy:update", "deploy:migrate"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
