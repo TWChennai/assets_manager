cd27eff4a2498bff65315e745f7590e35bba5789
Raj Kumar <rajkum@thoughtworks.com>
2015-07-16 18:36:51 +0530

    Adding deploy scripts for puma Selva/Raj

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require 'puma/capistrano'
 require 'bundler/capistrano'
 
 set :application, "tw_asset_manager"
-set :repository,  "git@github.com:TWChennai/assets_manager.git"
+set :repository,  "https://github.com/TWChennai/assets_manager.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -19,14 +19,35 @@ role :db,  "10.10.5.17"
 after "deploy:restart", "deploy:cleanup"
 after "deploy:update", "deploy:migrate"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :stage, 'production'
+
+set :shared_children, shared_children << 'tmp/sockets'
+ 
+puma_sock    = "unix://#{shared_path}/sockets/puma.sock"
+puma_control = "unix://#{shared_path}/sockets/pumactl.sock"
+puma_state   = "#{shared_path}/sockets/puma.state"
+puma_log     = "#{shared_path}/log/puma-#{stage}.log"
+ 
+namespace :deploy do
+  desc "Start the application"
+  task :start do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec puma -b '#{puma_sock}' -e #{stage} -t2:4 --control '#{puma_control}' -S #{puma_state} >> #{puma_log} 2>&1 &", :pty => false
+  end
+ 
+  desc "Stop the application"
+  task :stop do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec pumactl -S #{puma_state} stop"
+  end
+ 
+  desc "Restart the application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec pumactl -S #{puma_state} restart"
+  end
+ 
+  desc "Status of the application"
+  task :status, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{stage} && bundle exec pumactl -S #{puma_state} stats"
+  end
+end
+
